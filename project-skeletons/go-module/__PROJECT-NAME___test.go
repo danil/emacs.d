3400cac88a9588dbce9bfe8abaaf3e49690b7568
Danil Kutkevich <danil@kutkevich.org>
2021-03-20 12:53:35 +0300

    fixing project skeleton for the go module

--- project-skeletons/go-module/__PROJECT-NAME___test.go
+++ project-skeletons/go-module/__PROJECT-NAME___test.go
@@ -8,6 +8,6 @@ import (
 
 func TestHello(t *testing.T) {
 	if __PROJECT-NAME__.Hello() != "Hello, World!" {
-		t.Error("something went wrong")
+		t.Errorf("unexpected %q, expected %q", __PROJECT-NAME__.Hello(), "Hello, World!")
 	}
 }
